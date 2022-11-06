ef0e3cbd4cc918ee05cb0fe19e021ed469713ef5
Ashwin Sekar <50091931+acewinsekar@users.noreply.github.com>
2022-11-06 20:44:17 +0530

    Just testing and learning the utilities module and to read external urls

---
+++ google-python-exercises/util_funcs/utilities.py
@@ -0,0 +1,48 @@
+from urllib.request import urlopen
+import sys
+import re
+
+## Given a url, try to retrieve it. If it's text/html,
+## print its base url and its text.
+def wget(url):
+  ufile = urlopen(url)  ## get file-like object for url
+  info = ufile.info()   ## meta-info about the url content
+  print(info)
+  domain_name = re.search('https?://([A-Za-z_0-9-.]+).*', url)
+  #domain_name = re.search(r'https:\\\\(\w+)\.com', url)
+  if domain_name:
+      print('domain name extracted ' + domain_name.group(1))
+  else:
+      print('regex didnt work')
+
+  if info.get_content_type() == 'text/html':
+    print('base url:' + ufile.geturl())
+    text = ufile.read()  ## read all its text
+    print(text)
+    dm_name = domain_name.group(1)
+    outf = open(dm_name + '.txt', 'w') ## why is this write operation failing
+    # needs to be debugged
+    outf.write(text)
+    outf.close()
+
+
+def main():
+  # This basic command line argument parsing code is provided.
+  # Add code to call your functions below.
+
+  # Make a list of command line arguments, omitting the [0] element
+  # which is the script itself.
+  args = sys.argv[1:]
+  if not args:
+    print ("usage: enter url in https://www.url.com format")
+    sys.exit(1)
+
+  for url in args:
+      wget(url)
+
+  # +++your code here+++
+  # Call your functions
+
+
+if __name__ == '__main__':
+    main()
